ca39841c9a55b5104c6d37ad62f6fb272477cd87
Pavlo Kytsmey <pkyt@macmini42.local>
2014-01-22 13:45:39 +0200

    send_diff_format file added

---
+++ WebServerFirst/send_diff_format.h
@@ -0,0 +1,14 @@
+//
+//  send_diff_format.h
+//  WebServerFirst
+//
+//  Created by Pavlo Kytsmey on 1/22/14.
+//  Copyright (c) 2014 Pavlo Kytsmey. All rights reserved.
+//
+
+#ifndef WebServerFirst_send_diff_format_h
+#define WebServerFirst_send_diff_format_h
+
+
+
+#endif
